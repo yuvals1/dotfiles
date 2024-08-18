ae0d801db581194e477c42bcf87a60c3ebd0f269
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-08-18 18:15:18 +0300

    fix(copilot): fix copilot accept bug and remove prev and next suggestion due to buggy behaviour

--- .config/nvim/lua/custom/plugins/copilot.lua
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -3,14 +3,11 @@ return {
   config = function()
     -- Disable default Tab mapping for Copilot
     vim.g.copilot_no_tab_map = true
-
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_tab_fallback = ''
     -- Custom keybindings for Copilot
     vim.api.nvim_set_keymap('i', '<C-d>', '<Plug>(copilot-accept-word)', { silent = true })
-    vim.api.nvim_set_keymap('i', '<C-f>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    vim.api.nvim_set_keymap('i', '<C-]>', '<Plug>(copilot-dismiss)', { silent = true })
-    vim.api.nvim_set_keymap('i', '<M-]>', '<Plug>(copilot-next)', { silent = true })
-    vim.api.nvim_set_keymap('i', '<M-[>', '<Plug>(copilot-previous)', { silent = true })
-    vim.api.nvim_set_keymap('i', '<M-\\>', '<Plug>(copilot-suggest)', { silent = true })
+    vim.api.nvim_set_keymap('i', '<C-f>', 'copilot#Accept()', { silent = true, expr = true, script = true })
 
     -- You can add more custom configurations here
   end,
