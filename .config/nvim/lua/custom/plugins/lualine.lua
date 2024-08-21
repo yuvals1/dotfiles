529d2e0251e774d787eb2c3b33a27979b0bfdc24
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-08-21 22:11:10 +0300

    feat(nvim): modify lualine config

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -6,8 +6,9 @@ return {
       options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+
         disabled_filetypes = {
           statusline = {},
           winbar = {},
@@ -23,17 +24,28 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_b = { 'diff', 'diagnostics' },
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+          },
+        },
+        lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+        lualine_z = {},
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+          },
+        },
         lualine_y = {},
         lualine_z = {},
       },
