3c03ea12bb6b323cff85d9011286ef9e56475c14
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-08-22 17:34:31 +0300

    feat(nvim/iron): add cell execution functionality

--- .config/nvim/lua/custom/plugins/iron.lua
+++ .config/nvim/lua/custom/plugins/iron.lua
@@ -15,21 +15,25 @@ return {
         local win = vim.api.nvim_get_current_win()
         vim.wo[win].number = false
         vim.wo[win].relativenumber = false
-
-        -- Add Esc mapping for the REPL buffer
         vim.api.nvim_buf_set_keymap(bufnr, 't', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
-
         return win
       end
 
-      -- Custom function to toggle REPL
-      local function toggle_repl()
-        local ft = vim.bo.filetype
-        if ft == '' then
-          print 'No filetype detected'
-          return
+      -- Function to execute the current cell
+      local function execute_cell()
+        local start_line = vim.fn.search('^# %%', 'bnW')
+        local end_line = vim.fn.search('^# %%', 'nW') - 1
+        if end_line == -1 then
+          end_line = vim.fn.line '$'
         end
-        iron.repl_for(ft)
+
+        local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
+        lines = vim.tbl_filter(function(line)
+          return not line:match '^# %%'
+        end, lines)
+
+        local code = table.concat(lines, '\n')
+        iron.send(vim.bo.filetype, code)
       end
 
       iron.setup {
@@ -49,20 +53,19 @@ return {
           send_line = '<space>jl',
           send_until_cursor = '<space>jc',
           exit = '<space>jq',
-          -- send_motion = '<space>re',
           visual_send = '<space>jm',
         },
         highlight = { italic = true },
         ignore_blank_lines = true,
       }
 
-      -- Set up the toggle keymap outside of iron.setup
-      vim.api.nvim_set_keymap(
-        'n',
-        '<space>jj',
-        [[<cmd>lua require('iron.core').repl_for(vim.bo.filetype)<CR>]],
-        { noremap = true, silent = true, desc = 'Toggle REPL' }
-      )
+      -- Set up the toggle keymap
+      vim.keymap.set('n', '<space>jj', function()
+        iron.repl_for(vim.bo.filetype)
+      end, { noremap = true, silent = true, desc = 'Toggle REPL' })
+
+      -- Set up a keymap to execute the current cell
+      vim.keymap.set('n', '<space>jx', execute_cell, { noremap = true, silent = true, desc = 'Execute current cell' })
 
       -- Additional setup for better REPL experience
       vim.api.nvim_create_autocmd('TermOpen', {
@@ -71,7 +74,7 @@ return {
           vim.opt_local.number = false
           vim.opt_local.relativenumber = false
           vim.cmd 'startinsert'
-          vim.api.nvim_buf_set_keymap(0, 't', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+          vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { buffer = true, noremap = true, silent = true })
         end,
       })
 
@@ -82,10 +85,8 @@ return {
       vim.api.nvim_create_autocmd('FileType', {
         pattern = 'iron',
         callback = function()
-          -- Exit insert mode with jk
-          vim.api.nvim_buf_set_keymap(0, 'i', 'jk', '<Esc>', { noremap = true, silent = true })
-          -- Enter insert mode with i
-          vim.api.nvim_buf_set_keymap(0, 'n', 'i', 'i', { noremap = true, silent = true })
+          vim.keymap.set('i', 'jk', '<Esc>', { buffer = true, noremap = true, silent = true })
+          vim.keymap.set('n', 'i', 'i', { buffer = true, noremap = true, silent = true })
         end,
       })
 
