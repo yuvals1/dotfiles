68a8e925804a584de420dfe37bd58b3eaf95ddbe
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-08-24 00:31:51 +0300

    fix(nvim): fix duplicated markdown system rendering - remove markdown config in avante

--- .config/nvim/lua/custom/plugins/avante.lua
+++ .config/nvim/lua/custom/plugins/avante.lua
@@ -55,12 +55,12 @@ return {
       },
     },
     --- The below is optional, make sure to setup it properly if you have lazy=true
-    {
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { 'markdown', 'Avante' },
-      },
-      ft = { 'markdown', 'Avante' },
-    },
+    -- {
+    --   'MeanderingProgrammer/render-markdown.nvim',
+    --   opts = {
+    --     file_types = { 'markdown', 'Avante' },
+    --   },
+    --   ft = { 'markdown', 'Avante' },
+    -- },
   },
 }
