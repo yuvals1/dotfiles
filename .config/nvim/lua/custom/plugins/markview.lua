dc4cb61a4c17750da237c0482665e1e273898ef4
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-08-23 23:16:22 +0300

    feat(nvim): add markview plugin

---
+++ .config/nvim/lua/custom/plugins/markview.lua
@@ -0,0 +1,25 @@
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false, -- Recommended to not lazy-load this plugin
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('markview').setup {
+      -- Your configuration options here
+      -- For example:
+      modes = { 'n', 'no', 'c' },
+      hybrid_modes = { 'n' },
+      callbacks = {
+        on_enable = function(_, win)
+          vim.wo[win].conceallevel = 2
+          vim.wo[win].concealcursor = 'c'
+        end,
+      },
+    }
+
+    -- Optionally, you can enable Markview for all buffers here
+    -- vim.cmd("Markview enableAll")
+  end,
+}
