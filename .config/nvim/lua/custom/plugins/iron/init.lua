24836be7ea7611d92070c10f39d433b34bde58e1
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-01 14:11:01 +0300

    refactor(nvim/iron): move execution logic from init to executor

--- .config/nvim/lua/custom/plugins/iron/init.lua
+++ .config/nvim/lua/custom/plugins/iron/init.lua
@@ -1,27 +1,5 @@
 -- File: lua/custom/plugins/iron/init.lua
 
-local function execute_python_extractor(line_number, command)
-  local python_path = vim.g.python3_host_prog
-  local script_path = vim.fn.expand '~/.config/nvim/lua/custom/plugins/iron/code_block_extractor.py'
-
-  if vim.fn.filereadable(script_path) == 0 then
-    vim.api.nvim_echo({ { string.format('Error: Script not found at %s', script_path), 'ErrorMsg' } }, false, {})
-    return
-  end
-
-  local buffer_content = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n')
-  local tmp_file = vim.fn.tempname()
-  local tmp_fd = io.open(tmp_file, 'w')
-  tmp_fd:write(buffer_content)
-  tmp_fd:close()
-
-  local cmd = string.format('%s %s %d %s < %s', python_path, script_path, line_number, command, tmp_file)
-  local output = vim.fn.system(cmd)
-
-  os.remove(tmp_file)
-  return output
-end
-
 return {
   'Vigemus/iron.nvim',
   event = 'VeryLazy',
@@ -40,46 +18,6 @@ return {
 
     vim.api.nvim_create_user_command('IronCleanSigns', execution_tracker.clean_execution_marks, {})
     vim.api.nvim_create_user_command('IronClearAndRestart', repl.clear_and_restart, {})
-
-    -- Modify the executor.smart_execute function to use Python AST
-    executor.smart_execute = function()
-      if vim.bo.filetype ~= 'python' then
-        print 'This function is only for Python files.'
-        return
-      end
-
-      local current_line = vim.fn.line '.'
-      local code_block = execute_python_extractor(current_line, 'block')
-      local range = execute_python_extractor(current_line, 'range')
-      local start_line, end_line = range:match '(%d+),(%d+)'
-
-      repl.send_to_repl(code_block, tonumber(start_line), tonumber(end_line), 'smart')
-    end
-
-    -- Helper function to ensure there's a line to move to
-    local function ensure_next_line()
-      local last_line = vim.fn.line '$'
-      if vim.fn.line '.' == last_line then
-        vim.fn.append(last_line, '')
-        return true
-      end
-      return false
-    end
-
-    -- Modify the executor.smart_execute_and_move function
-    executor.smart_execute_and_move = function()
-      executor.smart_execute()
-      local range = execute_python_extractor(vim.fn.line '.', 'range')
-      local _, end_line = range:match '(%d+),(%d+)'
-      end_line = tonumber(end_line)
-
-      local created_new_line = ensure_next_line()
-      if created_new_line then
-        vim.api.nvim_win_set_cursor(0, { end_line + 1, 0 })
-      else
-        vim.api.nvim_win_set_cursor(0, { math.min(end_line + 1, vim.fn.line '$'), 0 })
-      end
-    end
 
     iron.setup {
       config = {
