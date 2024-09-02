921965e52b2b555c53ec310d0da91df99d6fe7c5
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-02 15:12:22 +0300

    feat(nvim/noice): modify keybind

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -75,7 +75,7 @@ return {
     vim.keymap.set('n', '<leader>nd', function()
       require('noice').cmd 'dismiss'
     end, { silent = true, desc = 'Dismiss Noice notifications' })
-    vim.keymap.set('n', '<C-i>', function()
+    vim.keymap.set('n', '<C-f>', function()
       require('noice').cmd 'dismiss'
     end, { silent = true, desc = 'Dismiss Noice notifications' })
   end,
