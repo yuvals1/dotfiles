47fbb7b4e9e6db22a3b1cd96a6b8deb6a0bf599d
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-03 10:16:40 +0300

    feat(nvim/yazi): more convinient keymaps

--- .config/nvim/lua/custom/plugins/yazi.lua
+++ .config/nvim/lua/custom/plugins/yazi.lua
@@ -12,15 +12,14 @@ return {
           show_help = '<f1>',
         },
       }
+      -- Resume the last yazi session
+      vim.keymap.set('n', '-', '<CMD>Yazi toggle<CR>', { desc = 'Resume last yazi session' })
 
       -- Open yazi at the current file
-      vim.keymap.set('n', '-', '<CMD>Yazi<CR>', { desc = 'Open yazi at the current file' })
+      vim.keymap.set('n', '<leader>-', '<CMD>Yazi<CR>', { desc = 'Open yazi at the current file' })
 
       -- Open the file manager in nvim's working directory
       vim.keymap.set('n', '<leader>cw', '<CMD>Yazi cwd<CR>', { desc = 'Open yazi in working directory' })
-
-      -- Resume the last yazi session
-      vim.keymap.set('n', '<c-up>', '<CMD>Yazi toggle<CR>', { desc = 'Resume last yazi session' })
     end,
   },
 }
