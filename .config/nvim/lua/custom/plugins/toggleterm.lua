92364a165df29196d952c6c5d00c6185b8e1eccc
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-06 12:41:06 +0000

    feat(nvim): add toggleterm

---
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      size = 20,
+      open_mapping = [[<C-t>]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = 'float',
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = 'curved',
+        winblend = 0,
+        highlights = {
+          border = 'Normal',
+          background = 'Normal',
+        },
+      },
+    },
+    config = function(_, opts)
+      require('toggleterm').setup(opts)
+
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      end
+
+      -- Auto command to set terminal keymaps when terminal is opened
+      vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+    end,
+  },
+}
