cdaaecb6df4d3d657ae1d532b7744ad94706d1de
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-11 22:42:54 +0300

    feat: make go to file work when there are spaces in the path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1058,3 +1058,42 @@ vim.api.nvim_set_keymap('n', '<leader>cf', ':CopyFullPath<CR>', { noremap = true
 
 -- Keymapping for CopyParentPath
 vim.api.nvim_set_keymap('n', '<leader>cp', ':CopyParentPath<CR>', { noremap = true, silent = true })
+
+-- fixing go to file not working when there are spaces in the path
+local function go_to_file()
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  local filepath
+
+  -- Try to match a filepath pattern
+  filepath = line:match '"([^"]+)"' or line:match "'([^']+)'" or line:match '`([^`]+)`'
+
+  if not filepath then
+    -- If no quotes found, try to extract path from current cursor position
+    local left = line:sub(1, col):reverse():find '[^%w%./\\-_]'
+    local right = line:sub(col + 1):find '[^%w%./\\-_]'
+    left = left and col - left + 1 or 1
+    right = right and col + right or #line
+    filepath = line:sub(left, right)
+  end
+
+  if filepath then
+    -- Expand the path (resolves '~' to home directory)
+    filepath = vim.fn.expand(filepath)
+
+    -- Check if the file exists
+    if vim.fn.filereadable(filepath) == 1 then
+      vim.cmd('edit ' .. vim.fn.fnameescape(filepath))
+    else
+      print('File not found: ' .. filepath)
+    end
+  else
+    print 'No file path found under cursor'
+  end
+end
+
+-- Create a user command
+vim.api.nvim_create_user_command('GoToFile', go_to_file, {})
+
+-- Map the function to gf
+vim.api.nvim_set_keymap('n', 'gf', ':GoToFile<CR>', { noremap = true, silent = true })
