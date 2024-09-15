acee538a5bd42665a32ceb1f75361c1a3ae7e44a
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-15 22:35:48 +0300

    bring yazi plugin from the my original dotfiles

--- .config/yazi-nvim_copy/lua/plugins/yazi.lua
+++ .config/yazi-nvim_copy/lua/plugins/yazi.lua
@@ -1,18 +1,25 @@
 return {
-	{
-		"mikavilpas/yazi.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- optional
-		},
-		opts = {
-			-- Example configuration (you can customize this)
-			open_for_directories = false,
-			floating_window_scaling_factor = 0.9,
-		},
-		keys = {
-			-- Example keymapping
-			{ "<leader>y", "<cmd>Yazi<cr>", desc = "Open Yazi" },
-		},
-	},
+  {
+    'mikavilpas/yazi.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('yazi').setup {
+        open_for_directories = false,
+        -- Enable these if you are using the latest version of yazi
+        -- use_ya_for_events_reading = true,
+        -- use_yazi_client_id_flag = true,
+        keymaps = {
+          show_help = '<f1>',
+        },
+      }
+      -- Resume the last yazi session
+      vim.keymap.set('n', '-', '<CMD>Yazi toggle<CR>', { desc = 'Resume last yazi session' })
+
+      -- Open yazi at the current file
+      vim.keymap.set('n', '<leader>-', '<CMD>Yazi<CR>', { desc = 'Open yazi at the current file' })
+
+      -- Open the file manager in nvim's working directory
+      vim.keymap.set('n', '<leader>cw', '<CMD>Yazi cwd<CR>', { desc = 'Open yazi in working directory' })
+    end,
+  },
 }
