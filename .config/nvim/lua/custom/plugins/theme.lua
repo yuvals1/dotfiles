8f19b696d12f3e92dc8ef243f962c495ddbf8235
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-15 17:54:53 +0300

    refactor: keep deconstructing init.lua 2

---
+++ .config/nvim/lua/custom/plugins/theme.lua
@@ -0,0 +1,27 @@
+return { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  'folke/tokyonight.nvim',
+  priority = 999, -- Make sure to load this before all the other start plugins.
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'tokyonight-night'
+
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+  end,
+}
+-- return {
+--   'catppuccin/nvim',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd 'colorscheme catppuccin'
+--   end,
+--   opts = { flavour = 'macchiato' },
+-- }
+
+-- return { 'catppuccin/nvim', name = 'catppuccin', opts = { flavour = 'macchiato' }, priority = 1000 }
