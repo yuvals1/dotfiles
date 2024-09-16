89c6973c9718d5eb27aeb2faca745f183f318196
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-16 13:40:39 +0300

    chore(nvim): formatting config

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,25 +1,26 @@
 -- Load options
-require("options")
-require("autocmds")
-require("keymaps")
-require("file-cmds")
+require 'options'
+require 'autocmds'
+require 'keymaps'
+require 'file-cmds'
+require 'python'
 
 -- Set leader key (optional, but recommended for the default keymapping)
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Set up plugins
-require("lazy").setup("plugins")
+require('lazy').setup 'plugins'
