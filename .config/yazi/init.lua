bead33e3e86388aaab314d33a7142409e6ba84af
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-17 21:52:04 +0300

    feat(yazi/plugins): add mactag plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,2 +1,14 @@
-require("git"):setup({})
-require("full-border"):setup()
+require('git'):setup {}
+require('full-border'):setup()
+require('mactag'):setup {
+  -- You can change the colors of the tags here
+  colors = {
+    Red = '#ee7b70',
+    Orange = '#f5bd5c',
+    Yellow = '#fbe764',
+    Green = '#91fc87',
+    Blue = '#5fa3f8',
+    Purple = '#cb88f8',
+    Gray = '#b5b5b9',
+  },
+}
