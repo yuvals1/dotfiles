035ca2a145e9e995f655f73d81b4394ed52f3790
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-17 18:18:40 +0300

    feat(nvim/venv-selector): improved plugin configuration and add keybinds

--- .config/nvim/lua/plugins/venv-selector.lua
+++ .config/nvim/lua/plugins/venv-selector.lua
@@ -2,16 +2,25 @@ return {
   'linux-cultist/venv-selector.nvim',
   dependencies = {
     'neovim/nvim-lspconfig',
-    'mfussenegger/nvim-dap',
-    'mfussenegger/nvim-dap-python', --optional
-    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+    'nvim-telescope/telescope.nvim',
+    'mfussenegger/nvim-dap-python',
   },
-  lazy = false,
-  branch = 'regexp', -- This is the regexp branch, use this for the new version
-  config = function()
-    require('venv-selector').setup()
-  end,
+  branch = 'regexp', -- Using the new version as recommended
+  opts = {
+    -- Your options go here
+    auto_refresh = false,
+    search_venv_managers = true,
+    search_workspace = true,
+    dap_enabled = true, -- Enable debugger support
+    parents = 2, -- Number of parent directories to search
+    name = { 'venv', '.venv' }, -- Multiple names to search for
+    notify_user_on_activate = true,
+  },
+  event = 'VeryLazy', -- Load the plugin when needed
   keys = {
-    { ',v', '<cmd>VenvSelect<cr>' },
+    -- Keymap to open VenvSelector to pick a venv
+    { '<leader>vs', '<cmd>VenvSelect<cr>', desc = 'Select VirtualEnv' },
+    -- Keymap to retrieve the venv from cache
+    { '<leader>vc', '<cmd>VenvSelectCached<cr>', desc = 'Select Cached VirtualEnv' },
   },
 }
