8657339f7c663a6f5df99df482191559e8662fce
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-18 12:16:23 +0300

    feat(zsh): add aliases for copying the filenames of the changed files

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -12,6 +12,7 @@ alias scivim="NVIM_APPNAME=scivim nvim"
 alias kickstart='NVIM_APPNAME=kickstart nvim'
 alias gpr='f() { git checkout $(git rev-list -n 1 --grep="#$1" HEAD) }; f'
 alias lg='lazygit --use-config-file="$HOME/.config/lazygit/config.yml"'
+
 # git diff to clipboard
 alias gdcopy='git diff | pbcopy'
 alias gdscopy='git diff --staged | pbcopy'
@@ -19,4 +20,13 @@ gdpcopy() {
   local commits=${1:-1}
   git diff HEAD~$commits HEAD | pbcopy
   echo "Copied diff of last $commits commit(s) to clipboard"
+}
+
+# Changed files aliases
+alias gfcopy='git diff --name-only | pbcopy'
+alias gfscopy='git diff --staged --name-only | pbcopy'
+gfpcopy() {
+  local commits=${1:-1}
+  git diff --name-only HEAD~$commits HEAD | pbcopy
+  echo "Copied names of files changed in last $commits commit(s) to clipboard"
 }
