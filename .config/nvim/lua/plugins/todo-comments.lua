50a6da2fe8556d48086f2ca138de013ee082775e
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-19 16:08:40 +0300

    feat(todo-comments): add DEBUG keyword and navigation mappings
    - Add DEBUG keyword with bug icon and error color
    - Include DEBUGGING and FIXME as alternative keywords
    - Implement ]t and [t mappings for navigating todo comments
    - Set signs to false in the configuration

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,4 +1,68 @@
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    signs = false,
+    keywords = {
+      TODO = { icon = ' ', color = 'info' },
+      HACK = { icon = ' ', color = 'warning' },
+      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+      PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+      NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+      DEBUG = { icon = ' ', color = 'error', alt = { 'DEBUGGING', 'FIXME' } },
+    },
+    highlight = {
+      multiline = true,
+      multiline_pattern = '^.',
+      multiline_context = 10,
+      before = '',
+      keyword = 'wide',
+      after = 'fg',
+      pattern = [[.*<(KEYWORDS)\s*:]],
+      comments_only = true,
+      max_line_len = 400,
+      exclude = {},
+    },
+    colors = {
+      error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
+      warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
+      info = { 'DiagnosticInfo', '#2563EB' },
+      hint = { 'DiagnosticHint', '#10B981' },
+      default = { 'Identifier', '#7C3AED' },
+      test = { 'Identifier', '#FF00FF' },
+    },
+    search = {
+      command = 'rg',
+      args = {
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+      },
+      pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    },
+  },
+  keys = {
+    {
+      ']t',
+      function()
+        require('todo-comments').jump_next()
+      end,
+      desc = 'Next todo comment',
+    },
+    {
+      '[t',
+      function()
+        require('todo-comments').jump_prev()
+      end,
+      desc = 'Previous todo comment',
+    },
+  },
+}
+
 -- todo comments variants:
 --TODO:
 --NOTE:
