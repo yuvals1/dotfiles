a276b646afe16b38a10d10f9ad854aca9d4e4fe9
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-20 17:44:42 +0300

    feat(nvim/gitsigns): modify keybindings

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -23,22 +23,20 @@ return {
         end
 
         -- Navigation
-        map('n', '<leader>]', function()
+        map('n', 'L', function()
           if vim.wo.diff then
             vim.cmd.normal { ']c', bang = true }
           else
             gitsigns.nav_hunk 'next'
           end
         end, { desc = 'Jump to next git [c]hange' })
-
-        map('n', '<leader>[', function()
+        map('n', 'H', function()
           if vim.wo.diff then
             vim.cmd.normal { '[c', bang = true }
           else
             gitsigns.nav_hunk 'prev'
           end
         end, { desc = 'Jump to previous git [c]hange' })
-
         -- Actions
         -- visual mode
         map('v', '<leader>hs', function()
@@ -48,21 +46,16 @@ return {
           gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'reset git hunk' })
         -- normal mode
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-        map('n', '<leader>1', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-        map('n', '<leader>0', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-        map('n', '<leader>9', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-        map('n', '<leader>2', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+        map('n', 'M', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+        map('n', 'R', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+        map('n', 'P', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+        map('n', '<leader>hs', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+        map('n', '<leader>hm', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
         map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+        map('n', '<leader>hr', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
         map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', '<leader>p', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', "<leader>'", gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
         map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
         map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-        map('n', '<leader>4', gitsigns.diffthis, { desc = 'git [d]iff against index' })
         map('n', '<leader>hD', function()
           gitsigns.diffthis '@'
         end, { desc = 'git [D]iff against last commit' })
