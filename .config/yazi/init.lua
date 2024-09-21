9f7cffbac1d49fc6ed950ad3bc58618a13f40fcc
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-21 04:30:22 +0300

    remove yazi mactag

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,17 +1,5 @@
 require('git'):setup {}
 require('full-border'):setup()
-require('mactag'):setup {
-  -- You can change the colors of the tags here
-  colors = {
-    Red = '#ee7b70',
-    Orange = '#f5bd5c',
-    Yellow = '#fbe764',
-    Green = '#91fc87',
-    Blue = '#5fa3f8',
-    Purple = '#cb88f8',
-    Gray = '#b5b5b9',
-  },
-}
 require('starship'):setup()
 
 -- You can configure your bookmarks by lua language
