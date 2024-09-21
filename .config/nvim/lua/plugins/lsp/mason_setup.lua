ffe224c229d04a60490320a45cf0f4b391dc6519
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-21 12:47:33 +0300

    feat(nvim/lsp): add servers to setup_mason.lua

--- .config/nvim/lua/plugins/lsp/mason_setup.lua
+++ .config/nvim/lua/plugins/lsp/mason_setup.lua
@@ -1,14 +1,18 @@
 local M = {}
-
 M.setup = function()
   require('mason').setup()
-
   local ensure_installed = vim.tbl_keys(require('plugins.lsp.servers').servers or {})
   vim.list_extend(ensure_installed, {
+    'bash-language-server',
+    'json-lsp',
+    'marksman',
+    'lemminx',
+    'yaml-language-server',
+    'lua-language-server',
+    'jedi-language-server',
     'stylua',
   })
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
   require('mason-lspconfig').setup {
     handlers = {
       function(server_name)
@@ -18,5 +22,4 @@ M.setup = function()
     },
   }
 end
-
 return M
