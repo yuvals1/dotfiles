6cb35d578e00073f3ed057c45b7b1f2862dfcfbc
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-22 01:24:29 +0300

    fix(nvim/plugins): fix keymap

--- .config/nvim/lua/plugins/advanced-git-search.lua
+++ .config/nvim/lua/plugins/advanced-git-search.lua
@@ -2,9 +2,7 @@ return {
   'aaronhallaert/advanced-git-search.nvim',
   cmd = { 'AdvancedGitSearch' },
   config = function()
-    -- optional: setup telescope before loading the extension
     require('telescope').setup {
-      -- move this to the place where you call the telescope setup function
       extensions = {
         advanced_git_search = {
           -- See Config
@@ -12,11 +10,11 @@ return {
       },
     }
     require('telescope').load_extension 'advanced_git_search'
-
-    -- Add keymap
-    vim.keymap.set('n', '<leader>sg', '<cmd>AdvancedGitSearch<CR>', { noremap = true, silent = true, desc = 'Advanced Git Search' })
   end,
   dependencies = {
     --- See dependencies
+  },
+  keys = {
+    { '<leader>sg', '<cmd>AdvancedGitSearch<CR>', desc = 'Advanced Git Search' },
   },
 }
