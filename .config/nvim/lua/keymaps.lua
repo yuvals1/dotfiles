156361f63f3e2af00c6500312da886997bfb97b5
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-22 12:04:35 +0300

    feat(keymaps): add new clipboard management functions
    - Remap 'yaa' to 'yaf' for yanking entire file
    - Add function to append file path and content to clipboard
    - Add function to clear clipboard
    - Add new keymaps:
      - 'yaa' to append file path and content to clipboard
      - 'ycc' to clear clipboard

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,5 +1,5 @@
--- Yank entire file
-vim.keymap.set('n', 'yaa', ':%y<CR>', { noremap = true, silent = true, desc = 'Yank entire file' })
+-- Yank entire file (remapped to avoid conflict)
+vim.keymap.set('n', 'yaf', ':%y<CR>', { noremap = true, silent = true, desc = 'Yank entire file' })
 
 -- Function to copy file path and content to clipboard
 local function copy_file_path_and_content()
@@ -7,6 +7,27 @@ local function copy_file_path_and_content()
   local file_content = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n')
   local clipboard_content = string.format('# %s\n%s', file_path, file_content)
   vim.fn.setreg('+', clipboard_content)
+end
+
+-- Function to append file path and content to clipboard
+local function append_file_path_and_content()
+  local file_path = vim.fn.fnamemodify(vim.fn.expand '%', ':.')
+  local file_content = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n')
+  local clipboard_content = string.format('# %s\n%s', file_path, file_content)
+  local current_clipboard = vim.fn.getreg '+'
+  if current_clipboard == nil or current_clipboard == '' then
+    -- Clipboard is empty, set the new content
+    vim.fn.setreg('+', clipboard_content)
+  else
+    -- Append to existing clipboard content
+    vim.fn.setreg('+', current_clipboard .. '\n\n' .. clipboard_content)
+  end
+end
+
+-- Function to clear the clipboard
+local function clear_clipboard()
+  vim.fn.setreg('+', '')
+  print 'Clipboard cleared'
 end
 
 -- Function to highlight the entire buffer
@@ -31,9 +52,21 @@ local function highlight_entire_buffer()
   end, 150)
 end
 
--- Keymap to copy file path and content, and highlight the buffer
+-- Keymap to copy file path and content (replacing clipboard)
 vim.keymap.set('n', 'yac', function()
   copy_file_path_and_content()
   highlight_entire_buffer()
   print 'File path and content copied to clipboard'
 end, { noremap = true, silent = true, desc = 'Copy file path and content to clipboard' })
+
+-- Keymap to append file path and content to clipboard
+vim.keymap.set('n', 'yaa', function()
+  append_file_path_and_content()
+  highlight_entire_buffer()
+  print 'File path and content appended to clipboard'
+end, { noremap = true, silent = true, desc = 'Append file path and content to clipboard' })
+
+-- Keymap to clear the clipboard
+vim.keymap.set('n', 'ycc', function()
+  clear_clipboard()
+end, { noremap = true, silent = true, desc = 'Clear clipboard' })
