450963e766350068039e2eee7f60fb75bdeb5214
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-27 18:46:43 +0300

    perf(telescope): optimize lazy loading and startup performance
    - Replace 'VimEnter' event with 'cmd' for lazy loading
    - Mark all dependencies as lazy
    - Defer Telescope setup using vim.defer_fn
    - Implement on-demand extension loading
    - Consolidate configuration into a single return statement
    
    This commit aims to significantly reduce Telescope's impact on Neovim's
    startup time by delaying the loading of Telescope and its dependencies
    until they are actually needed.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,65 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  event = 'VimEnter',
-  branch = '0.1.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-      cond = function()
-        return vim.fn.executable 'make' == 1
-      end,
-    },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
-    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-  },
-  config = function()
-    local telescope = require 'telescope'
-    local builtin = require 'telescope.builtin'
-
-    telescope.setup {
-      defaults = {
-        path_display = function(opts, path)
-          local tail = require('telescope.utils').path_tail(path)
-          local relative_path = vim.fn.fnamemodify(path, ':~:.')
-          if relative_path ~= tail then
-            return string.format('%s |       %s', tail, relative_path), { { { 1, #tail }, 'Constant' } }
-          else
-            return tail, { { { 1, #tail }, 'Constant' } }
-          end
-        end,
-        layout_strategy = 'vertical',
-        layout_config = {
-          vertical = {
-            width = 0.8,
-            height = 0.9,
-            preview_height = 0.5,
-            preview_cutoff = 0,
-          },
-        },
-      },
-      pickers = {
-        find_files = {
-          hidden = true,
-          find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
-        },
-        live_grep = {
-          additional_args = function()
-            return { '--hidden' }
-          end,
-        },
-      },
-      extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
-        },
-      },
-    }
-
-    pcall(telescope.load_extension, 'fzf')
-    pcall(telescope.load_extension, 'ui-select')
-  end,
+  cmd = 'Telescope',
   keys = {
     {
       '<leader>sh',
@@ -152,4 +93,74 @@ return {
       desc = '[S]earch [N]eovim files',
     },
   },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', lazy = true },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      lazy = true,
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
+    { 'nvim-telescope/telescope-ui-select.nvim', lazy = true },
+    { 'nvim-tree/nvim-web-devicons', lazy = true, enabled = vim.g.have_nerd_font },
+  },
+  config = function()
+    vim.defer_fn(function()
+      local telescope = require 'telescope'
+      local builtin = require 'telescope.builtin'
+      telescope.setup {
+        defaults = {
+          path_display = function(opts, path)
+            local tail = require('telescope.utils').path_tail(path)
+            local relative_path = vim.fn.fnamemodify(path, ':~:.')
+            if relative_path ~= tail then
+              return string.format('%s |       %s', tail, relative_path), { { { 1, #tail }, 'Constant' } }
+            else
+              return tail, { { { 1, #tail }, 'Constant' } }
+            end
+          end,
+          layout_strategy = 'vertical',
+          layout_config = {
+            vertical = {
+              width = 0.8,
+              height = 0.9,
+              preview_height = 0.5,
+              preview_cutoff = 0,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+          },
+          live_grep = {
+            additional_args = function()
+              return { '--hidden' }
+            end,
+          },
+        },
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown(),
+          },
+        },
+      }
+
+      -- Lazy load extensions
+      local function load_extension(name)
+        return function()
+          telescope.load_extension(name)
+          telescope.extensions[name][name]()
+        end
+      end
+
+      -- You can call these functions when you need to use the extensions
+      -- For example, you could add them to your keymaps:
+      -- vim.keymap.set('n', '<leader>sf', load_extension('fzf'), { desc = 'Telescope FZF' })
+      -- vim.keymap.set('n', '<leader>su', load_extension('ui-select'), { desc = 'Telescope UI Select' })
+    end, 100)
+  end,
 }
