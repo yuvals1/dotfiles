8aed1f4085fd229e714ce1d4d440132e0ba40355
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-29 23:19:44 +0300

    feat(nvim/plugins): add format_on_save to conform

--- .config/nvim/lua/plugins/languages/conform.lua
+++ .config/nvim/lua/plugins/languages/conform.lua
@@ -9,6 +9,10 @@ function M.setup(languages)
     cmd = {"ConformInfo"},
     opts = {
       formatters_by_ft = configs.formatters,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
     },
   }
 end
