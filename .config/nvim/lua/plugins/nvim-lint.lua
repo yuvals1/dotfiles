47cfac8a380bc1bcc5b06a85a00b3285eb096901
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-29 23:25:48 +0300

    chore: comment out old conform, lsp, nvim-lint files

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,22 +1,22 @@
 return {
-  { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-        python = {}, -- Add ruff and mypy here
-        go = { 'golangci_lint' }, -- Add this line for Golang
-      }
-
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
+  -- { -- Linting
+  --   'mfussenegger/nvim-lint',
+  --   event = { 'BufReadPre', 'BufNewFile' },
+  --   config = function()
+  --     local lint = require 'lint'
+  --     lint.linters_by_ft = {
+  --       markdown = { 'markdownlint' },
+  --       python = {}, -- Add ruff and mypy here
+  --       go = { 'golangci_lint' }, -- Add this line for Golang
+  --     }
+  --
+  --     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+  --     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+  --       group = lint_augroup,
+  --       callback = function()
+  --         lint.try_lint()
+  --       end,
+  --     })
+  --   end,
+  -- },
 }
