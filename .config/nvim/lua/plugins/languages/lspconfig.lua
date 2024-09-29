42cdd0f95e1ab14a1443ebf896508e2a8dc8dde0
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-29 23:51:02 +0300

    fix(neovim): correct LSP setup order to prevent startup errors
    - Change mason-lspconfig.nvim event to load earlier
    - Add mason-lspconfig.nvim as a dependency for nvim-lspconfig
    - Ensure language servers are installed before LSP setup

--- .config/nvim/lua/plugins/languages/lspconfig.lua
+++ .config/nvim/lua/plugins/languages/lspconfig.lua
@@ -2,26 +2,27 @@ local language_utils = require('plugins.languages.language_utils')
 local M = {}
 
 function M.setup(languages)
-  local configs = language_utils.collect_configurations(languages)
-  return {
-    {
-      'williamboman/mason-lspconfig.nvim',
-      event = "VeryLazy",
-      opts = {
-        ensure_installed = vim.tbl_keys(configs.lsp_servers),
-      },
-    },
-    {
-      'neovim/nvim-lspconfig',
-      event = {"BufReadPre", "BufNewFile"},
-      config = function()
-        local lspconfig = require('lspconfig')
-        for server, config in pairs(configs.lsp_servers) do
-          lspconfig[server].setup(config)
-        end
-      end,
-    },
-  }
+	local configs = language_utils.collect_configurations(languages)
+	return {
+		{
+			'williamboman/mason-lspconfig.nvim',
+			event = { "BufReadPre", "BufNewFile" },
+			opts = {
+				ensure_installed = vim.tbl_keys(configs.lsp_servers),
+			},
+		},
+		{
+			'neovim/nvim-lspconfig',
+			dependencies = { 'williamboman/mason-lspconfig.nvim' },
+			event = { "BufReadPre", "BufNewFile" },
+			config = function()
+				local lspconfig = require('lspconfig')
+				for server, config in pairs(configs.lsp_servers) do
+					lspconfig[server].setup(config)
+				end
+			end,
+		},
+	}
 end
 
 return M
