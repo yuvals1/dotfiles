70136d5bec74562e619a977605dfaa20b507c83f
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-30 14:58:58 +0300

    feat(nvim/lsp): add format buffer keymap

--- .config/nvim/lua/plugins/languages/conform.lua
+++ .config/nvim/lua/plugins/languages/conform.lua
@@ -1,12 +1,23 @@
-local language_utils = require('plugins.languages.language_utils')
+local language_utils = require 'plugins.languages.language_utils'
 local M = {}
 
 function M.setup(languages)
   local configs = language_utils.collect_configurations(languages)
   return {
     'stevearc/conform.nvim',
-    event = {"BufWritePre"},
-    cmd = {"ConformInfo"},
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>fc',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+
     opts = {
       formatters_by_ft = configs.formatters,
       format_on_save = {
