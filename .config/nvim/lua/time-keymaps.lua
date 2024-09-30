8d9deca7ad6ec2ccff0d6ffb819b01253818deb4
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-09-30 13:24:08 +0300

    chore: modify time format for t1

--- .config/nvim/lua/time-keymaps.lua
+++ .config/nvim/lua/time-keymaps.lua
@@ -10,7 +10,7 @@ local function insert_time(format)
 end
 
 -- Define mappings
-vim.keymap.set('n', '<leader>t1', insert_time '%H:%M:%S', { desc = 'Insert time (HH:MM:SS)' })
+vim.keymap.set('n', '<leader>t1', insert_time '%H:%M', { desc = 'Insert time (HH:MM:SS)' })
 vim.keymap.set('n', '<leader>t2', insert_time '%Y-%m-%d', { desc = 'Insert date (YYYY-MM-DD)' })
 vim.keymap.set('n', '<leader>t3', insert_time '%Y-%m-%d %H:%M:%S', { desc = 'Insert date and time' })
 vim.keymap.set('n', '<leader>t4', insert_time '%c', { desc = 'Insert full date and time' })
