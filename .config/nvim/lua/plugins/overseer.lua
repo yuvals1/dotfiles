0c7608b9703b2335bcddc64aed9d13d5955964d1
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-02 22:27:48 +0300

    feat(nvim/overseer): add keybinds and options

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -15,17 +15,15 @@ return {
   },
   keys = {
     -- You can add custom keybindings here if desired
-    -- { "<leader>ot", "<cmd>OverseerToggle<cr>", desc = "Toggle Overseer" },
-    -- { "<leader>or", "<cmd>OverseerRun<cr>", desc = "Run Overseer Task" },
+    { '<leader>ot', '<cmd>OverseerToggle<cr>', desc = 'Toggle Overseer' },
+    { '<leader>or', '<cmd>OverseerRun<cr>', desc = 'Run Overseer Task' },
   },
   opts = {
-    -- Your Overseer configuration options go here
-    -- For example:
-    -- task_list = {
-    --   direction = "bottom",
-    --   min_height = 25,
-    --   max_height = 25,
-    --   default_detail = 1,
-    -- },
+    task_list = {
+      direction = 'bottom',
+      min_height = 25,
+      max_height = 25,
+      default_detail = 1,
+    },
   },
 }
