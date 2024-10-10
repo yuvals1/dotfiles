fd09fdab1bdf1ecda0ee275b078915c14ae7aa49
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-10 11:10:58 +0300

    fix(nvim): remove Brewfile to Ruby filetype association
    This commit comments out the autocmd that sets Brewfile's filetype to Ruby.
    The previous association was interfering with the hop.nvim plugin's
    functionality, specifically the 'f' keybinding for word hopping in Brewfiles.
    
    By removing this association, we allow hop.nvim to work as expected in
    Brewfiles, improving navigation consistency across different file types.

--- .config/nvim/lua/plugins/lsp-and-tools/init.lua
+++ .config/nvim/lua/plugins/lsp-and-tools/init.lua
@@ -20,12 +20,13 @@ local highlight = require 'plugins.lsp-and-tools.highlight'
 vim.opt.updatetime = 500
 
 -- brewfile is a ruby file
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-  pattern = { 'Brewfile' },
-  callback = function()
-    vim.bo.filetype = 'ruby'
-  end,
-})
+-- vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+--   pattern = { 'Brewfile' },
+--   callback = function()
+--     vim.bo.filetype = 'ruby'
+--   end,
+-- })
+
 -- Function to set up highlighting for each language server
 local function setup_highlighting(client, bufnr)
   if client.server_capabilities.documentHighlightProvider then
