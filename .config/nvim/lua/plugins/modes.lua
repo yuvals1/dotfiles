a2d07ed7e70197e6bb5529ed727be3a9bcd53edd
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-12 11:23:31 +0300

    refactor(nvim): replace modes.nvim with modicator.nvim
    - Comment out the entire configuration for modes.nvim plugin
    - Add new configuration for modicator.nvim plugin
    
    The modicator.nvim setup includes:
    - Dependency on onedark.nvim colorscheme
    - Lazy loading on BufReadPost, BufNewFile, and VeryLazy events
    - Initialization of required Neovim options (cursorline, number, termguicolors)
    - Configuration to show warnings for missing required options
    
    This change aims to provide an alternative approach to enhancing
    Neovim's visual mode feedback.

--- .config/nvim/lua/plugins/modes.lua
+++ .config/nvim/lua/plugins/modes.lua
@@ -1,20 +1,20 @@
 return {
-  'mvllow/modes.nvim',
-  tag = 'v0.2.0',
-  event = 'VeryLazy',
-  config = function()
-    require('modes').setup {
-      colors = {
-        copy = '#f5c359',
-        delete = '#c75c6a',
-        insert = '#78ccc5',
-        visual = '#9745be',
-      },
-      line_opacity = 0.15,
-      set_cursor = true,
-      set_cursorline = true,
-      set_number = true,
-      ignore_filetypes = { 'NvimTree', 'TelescopePrompt' },
-    }
-  end,
+  -- 'mvllow/modes.nvim',
+  -- tag = 'v0.2.0',
+  -- event = 'VeryLazy',
+  -- config = function()
+  --   require('modes').setup {
+  --     colors = {
+  --       copy = '#f5c359',
+  --       delete = '#c75c6a',
+  --       insert = '#78ccc5',
+  --       visual = '#9745be',
+  --     },
+  --     line_opacity = 0.15,
+  --     set_cursor = true,
+  --     set_cursorline = true,
+  --     set_number = true,
+  --     ignore_filetypes = { 'NvimTree', 'TelescopePrompt' },
+  --   }
+  -- end,
 }
