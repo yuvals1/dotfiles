779d0defac9ac69dd94f91a33245f0905debc825
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-12 11:33:19 +0300

    chore(nvim/plugins): comment out oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,25 +1,25 @@
 -- lua/plugins/oil.lua
 return {
-  'stevearc/oil.nvim',
-  cmd = 'Oil',
-  keys = {
-    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
-  },
-  opts = {
-    -- Your oil.nvim options here
-    -- For example:
-    -- default_file_explorer = true,
-    -- skip_confirm_for_simple_edits = false,
-    -- prompt_save_on_select_new_entry = true,
-  },
-  dependencies = {
-    {
-      'echasnovski/mini.icons',
-      lazy = true,
-      opts = {},
-    },
-  },
-  config = function(_, opts)
-    require('oil').setup(opts)
-  end,
+  -- 'stevearc/oil.nvim',
+  -- cmd = 'Oil',
+  -- keys = {
+  --   { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
+  -- },
+  -- opts = {
+  --   -- Your oil.nvim options here
+  --   -- For example:
+  --   -- default_file_explorer = true,
+  --   -- skip_confirm_for_simple_edits = false,
+  --   -- prompt_save_on_select_new_entry = true,
+  -- },
+  -- dependencies = {
+  --   {
+  --     'echasnovski/mini.icons',
+  --     lazy = true,
+  --     opts = {},
+  --   },
+  -- },
+  -- config = function(_, opts)
+  --   require('oil').setup(opts)
+  -- end,
 }
