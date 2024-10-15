4a475a2e85942f2ea33f9ae6751cf6dff2a6f1a3
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-15 22:09:02 +0300

    feat(nvim/plugins): wcomment out hurl

--- .config/nvim/lua/plugins/hurl.lua
+++ .config/nvim/lua/plugins/hurl.lua
@@ -1,62 +1,62 @@
 return {
-  {
-    'jellydn/hurl.nvim',
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    ft = 'hurl',
-    opts = {
-      -- Show debugging info
-      debug = false,
-      -- Show notification on run
-      show_notification = false,
-      -- Show response in popup or split
-      mode = 'split',
-      -- Default formatter
-      formatters = {
-        json = { 'jq' },
-        html = {
-          'prettier',
-          '--parser',
-          'html',
-        },
-        xml = {
-          'tidy',
-          '-xml',
-          '-i',
-          '-q',
-        },
-      },
-      -- Default mappings for the response popup or split views
-      mappings = {
-        close = 'q',
-        next_panel = '<C-n>',
-        prev_panel = '<C-p>',
-      },
-      -- Set the environment file to ../.yuval.env
-      env_file = { '../.yuval.env' },
-    },
-    keys = {
-      {
-        '<leader>A',
-        function()
-          vim.cmd 'HurlRunnerAt --file-root /Users/yuvals1/data'
-        end,
-        desc = 'Run Api request',
-      },
-      {
-        '<leader>a',
-        function()
-          vim.cmd 'HurlRunner --file-root /Users/yuvals1/data'
-        end,
-        desc = 'Run All requests',
-      },
-      { '<leader>te', '<cmd>HurlRunnerToEntry<CR>', desc = 'Run Api request to entry' },
-      { '<leader>tm', '<cmd>HurlToggleMode<CR>', desc = 'Hurl Toggle Mode' },
-      { '<leader>tv', '<cmd>HurlVerbose<CR>', desc = 'Run Api in verbose mode' },
-      { '<leader>h', ':HurlRunner<CR>', desc = 'Hurl Runner', mode = 'v' },
-    },
-  },
+  -- {
+  --   'jellydn/hurl.nvim',
+  --   dependencies = {
+  --     'MunifTanjim/nui.nvim',
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --   },
+  --   ft = 'hurl',
+  --   opts = {
+  --     -- Show debugging info
+  --     debug = false,
+  --     -- Show notification on run
+  --     show_notification = false,
+  --     -- Show response in popup or split
+  --     mode = 'split',
+  --     -- Default formatter
+  --     formatters = {
+  --       json = { 'jq' },
+  --       html = {
+  --         'prettier',
+  --         '--parser',
+  --         'html',
+  --       },
+  --       xml = {
+  --         'tidy',
+  --         '-xml',
+  --         '-i',
+  --         '-q',
+  --       },
+  --     },
+  --     -- Default mappings for the response popup or split views
+  --     mappings = {
+  --       close = 'q',
+  --       next_panel = '<C-n>',
+  --       prev_panel = '<C-p>',
+  --     },
+  --     -- Set the environment file to ../.yuval.env
+  --     env_file = { '../.yuval.env' },
+  --   },
+  --   keys = {
+  --     {
+  --       '<leader>A',
+  --       function()
+  --         vim.cmd 'HurlRunnerAt --file-root /Users/yuvals1/data'
+  --       end,
+  --       desc = 'Run Api request',
+  --     },
+  --     {
+  --       '<leader>a',
+  --       function()
+  --         vim.cmd 'HurlRunner --file-root /Users/yuvals1/data'
+  --       end,
+  --       desc = 'Run All requests',
+  --     },
+  --     { '<leader>te', '<cmd>HurlRunnerToEntry<CR>', desc = 'Run Api request to entry' },
+  --     { '<leader>tm', '<cmd>HurlToggleMode<CR>', desc = 'Hurl Toggle Mode' },
+  --     { '<leader>tv', '<cmd>HurlVerbose<CR>', desc = 'Run Api in verbose mode' },
+  --     { '<leader>h', ':HurlRunner<CR>', desc = 'Hurl Runner', mode = 'v' },
+  --   },
+  -- },
 }
