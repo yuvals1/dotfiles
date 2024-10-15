08857169629ef8b3dcd36e7348cdf47195ca05cf
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-15 18:23:03 +0300

    feat(copilot): add toggle functionality and improve keybindings
    - Add function to toggle Copilot on/off
    - Create keymapping to trigger toggle function
    - Adjust Copilot keybindings for better usability
    - Remove commented out code

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -5,17 +5,30 @@ return {
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
     vim.g.copilot_tab_fallback = ''
-    -- Custom keybindings for Copilot
-    -- vim.api.nvim_set_keymap('i', '<C-d>', '<Plug>(copilot-accept-word)', { silent = true })
-    vim.api.nvim_set_keymap('i', '<C-e>', 'copilot#AcceptWord()', { silent = true, expr = true, script = true })
 
+    -- Custom keybindings for Copilot
+    vim.api.nvim_set_keymap('i', '<C-e>', 'copilot#AcceptWord()', { silent = true, expr = true, script = true })
     vim.api.nvim_set_keymap('i', '<C-d>', 'copilot#Accept()', { silent = true, expr = true, script = true })
+
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'jupyter',
       callback = function()
         vim.api.nvim_buf_set_keymap(0, 'i', '<C-d>', 'copilot#AcceptWord()', { silent = true, expr = true, script = true })
       end,
     })
-    -- You can add more custom configurations here
+
+    -- Function to toggle Copilot
+    function _G.toggle_copilot()
+      if vim.g.copilot_enabled == 1 then
+        vim.cmd 'Copilot disable'
+        print 'Copilot Disabled'
+      else
+        vim.cmd 'Copilot enable'
+        print 'Copilot Enabled'
+      end
+    end
+
+    -- Key mapping to toggle Copilot
+    vim.api.nvim_set_keymap('n', '<leader>tc', ':lua toggle_copilot()<CR>', { noremap = true, silent = true })
   end,
 }
