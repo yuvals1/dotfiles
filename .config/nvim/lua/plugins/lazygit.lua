f75284c9d21eae3ec5391c296a3f7d9188183be6
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-17 02:11:24 +0300

    feat(lazygit): expand configuration and keybindings
    - Add more keybindings for LazyGit commands
    - Configure custom LazyGit config file path
    - Change main keybinding prefix from 'g' to 'l'

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -15,6 +15,12 @@ return {
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    { '<leader>lc', '<cmd>LazyGitConfig<cr>', desc = 'LazyGitConfig' },
+    { '<leader>lf', '<cmd>LazyGitFilterCurrentFile<cr>', desc = 'LazyGitFilterCurrentFile' },
   },
+  config = function()
+    vim.g.lazygit_use_custom_config_file_path = 1
+    vim.g.lazygit_config_file_path = '~/.config/lazygit/config.yml'
+  end,
 }
