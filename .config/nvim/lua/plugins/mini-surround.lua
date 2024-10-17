b9e5741dd382e9ea85d0cf94d39c28fd3fe525ab
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-17 23:00:41 +0300

    fix(nvim/plugins): remove mini.surround setup to resolve hop keybinding conflicts
    - Remove mini.surround.setup() call to resolve interference with hop keybindings
    - Retain custom 't' keybinding for mini.surround in visual mode

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -5,10 +5,6 @@ return {
     -- Load the mini.surround module
     local mini_surround = require 'mini.surround'
 
-    -- Set up the module with default or empty config
-    mini_surround.setup {}
-
-    -- Create a custom mapping for Visual mode using Lua function
     vim.keymap.set('x', 't', function()
       mini_surround.add 'visual'
     end, { noremap = true, silent = true })
