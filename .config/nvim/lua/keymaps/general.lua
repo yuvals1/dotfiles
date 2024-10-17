ad7e2ca3297d4e30c59199c00bf8205fff2078f2
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-10-17 07:13:21 +0300

    refactor(nvim/keymaps): simplify delete-to-word-start functionality
    - Remove custom DeleteToWordStart() function
    - Replace with built-in Vim commands for deleting to word start
    - Add mapping for normal mode using 'db'
    - Update insert mode mapping to use '<Esc>dbi'

--- .config/nvim/lua/keymaps/general.lua
+++ .config/nvim/lua/keymaps/general.lua
@@ -15,4 +15,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Normal mode: delete to start of word
 vim.keymap.set('n', '<M-BS>', 'db', { noremap = true, silent = true })
+
+-- Insert mode: delete to start of word
+vim.keymap.set('i', '<M-BS>', '<Esc>dbi', { noremap = true, silent = true })
