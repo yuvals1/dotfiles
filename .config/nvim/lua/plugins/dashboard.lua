07c6f7893112dd088307e5e9006e3e26b7ebcc06
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-11-03 13:56:17 +0200

    fix(nvim): dashboard bug

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -3,7 +3,27 @@ return {
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      -- config
+      theme = 'hyper',
+      config = {
+        project = {
+          enable = false, -- Temporarily disable project feature
+          limit = 8,
+          icon = ' ',
+          label = '',
+          action = function()
+            require('telescope.builtin').find_files()
+          end,
+        },
+        shortcut = {
+          {
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+        },
+        footer = {}, -- Empty footer
+      },
     }
   end,
   dependencies = {
