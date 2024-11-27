3bc6f195c7d3937156c60789d2863243dfaa4956
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-11-27 11:14:58 +0200

    feat(debugger): enhance nvim-dap configuration and keymaps
    - Add nvim-dap-virtual-text for inline variable display
    - Configure dapui with customized controls and floating window
    - Add visual debugging signs with emojis
    - Expand keymaps for conditional breakpoints and UI interactions
    - Add Python-specific test debugging commands

--- .config/nvim/lua/plugins/debbuger.lua
+++ .config/nvim/lua/plugins/debbuger.lua
@@ -2,16 +2,55 @@ return {
   {
     'mfussenegger/nvim-dap',
     dependencies = {
-      'nvim-neotest/nvim-nio', -- Add this required dependency
+      'nvim-neotest/nvim-nio',
       'rcarriga/nvim-dap-ui',
       'mfussenegger/nvim-dap-python',
+      'theHamsta/nvim-dap-virtual-text', -- Show variable values as virtual text
     },
     config = function()
       local dap = require 'dap'
       local dapui = require 'dapui'
 
+      -- Enable virtual text
+      require('nvim-dap-virtual-text').setup {
+        enabled = true,
+        enabled_commands = true,
+        highlight_changed_variables = true,
+        highlight_new_as_changed = false,
+        show_stop_reason = true,
+        commented = false,
+        virt_text_pos = 'eol',
+        all_frames = false,
+        virt_lines = false,
+        virt_text_win_col = nil,
+      }
+
       -- DAP UI setup
-      dapui.setup()
+      dapui.setup {
+        controls = {
+          element = 'repl',
+          enabled = true,
+          -- icons = {
+          --   disconnect = '',
+          --   pause = '',
+          --   play = '',
+          --   run_last = '',
+          --   step_back = '',
+          --   step_into = '',
+          --   step_out = '',
+          --   step_over = '',
+          --   terminate = '',
+          -- },
+        },
+        floating = {
+          max_height = nil,
+          max_width = nil,
+          border = 'single',
+          mappings = {
+            close = { 'q', '<Esc>' },
+          },
+        },
+      }
 
       -- Dap firing events
       dap.listeners.after.event_initialized['dapui_config'] = function()
@@ -25,15 +64,55 @@ return {
       end
 
       -- Python setup
-      require('dap-python').setup 'python' -- Use python from PATH
+      require('dap-python').setup 'python'
+
+      -- Signs setup
+      vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapBreakpointCondition', { text = '⭕️', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapLogPoint', { text = '📝', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapStopped', { text = '⭐️', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapBreakpointRejected', { text = '❌', texthl = '', linehl = '', numhl = '' })
 
       -- Key mappings
-      vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-      vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Continue' })
-      vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
-      vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'Debug: Step Over' })
-      vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = 'Debug: Open REPL' })
-      vim.keymap.set('n', '<leader>ds', dap.terminate, { desc = 'Debug: Stop' })
+      local keymap = vim.keymap.set
+
+      -- Basic debugging
+      keymap('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+      keymap('n', '<leader>dB', function()
+        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end, { desc = 'Debug: Set Conditional Breakpoint' })
+      keymap('n', '<leader>dl', function()
+        dap.set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+      end, { desc = 'Debug: Set Log Point' })
+      keymap('n', '<leader>dc', dap.continue, { desc = 'Debug: Continue' })
+      keymap('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
+      keymap('n', '<leader>do', dap.step_over, { desc = 'Debug: Step Over' })
+      keymap('n', '<leader>dO', dap.step_out, { desc = 'Debug: Step Out' })
+      keymap('n', '<leader>ds', dap.terminate, { desc = 'Debug: Stop' })
+
+      -- REPL
+      keymap('n', '<leader>dr', dap.repl.open, { desc = 'Debug: Open REPL' })
+
+      -- UI
+      keymap('n', '<leader>dh', function()
+        require('dap.ui.widgets').hover()
+      end, { desc = 'Debug: Hover Variables' })
+      keymap('n', '<leader>dp', function()
+        require('dap.ui.widgets').preview()
+      end, { desc = 'Debug: Preview' })
+      keymap('n', '<leader>df', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.frames)
+      end, { desc = 'Debug: Show Frames' })
+      keymap('n', '<leader>dS', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.scopes)
+      end, { desc = 'Debug: Show Scopes' })
+
+      -- Python specific
+      keymap('n', '<leader>dtm', require('dap-python').test_method, { desc = 'Debug: Test Method' })
+      keymap('n', '<leader>dtc', require('dap-python').test_class, { desc = 'Debug: Test Class' })
+      keymap('n', '<leader>dts', require('dap-python').debug_selection, { desc = 'Debug: Selection' })
     end,
   },
 }
