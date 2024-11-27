ce041c0ccaefd50a895adc0ca7f9ac16d5fb74c6
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-11-27 10:19:40 +0200

    feat(nvim/plugins): add debug for nvim

---
+++ .config/nvim/lua/plugins/debbuger.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'mfussenegger/nvim-dap-python',
+    },
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      -- DAP UI setup
+      dapui.setup()
+
+      -- Dap firing events
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+
+      -- Python setup
+      require('dap-python').setup 'python' -- Use python from PATH
+
+      -- Key mappings
+      vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+      vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Continue' })
+      vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = 'Debug: Open REPL' })
+    end,
+  },
+}
