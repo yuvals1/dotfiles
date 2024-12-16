91263c8fbba9e75dc320dd46f1be59177bdc01c8
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-12-16 12:56:22 +0200

    fix: copilot.lua bug

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,5 @@
 return {
   'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
   event = 'InsertEnter',
   config = function()
     require('copilot').setup {
@@ -8,24 +7,17 @@ return {
         enabled = true,
         auto_trigger = true,
         keymap = {
-          -- Equivalent to your <C-d> mapping for accepting the whole suggestion
           accept = '<C-d>',
-          -- Equivalent to your <C-e> mapping for accepting a word
           accept_word = '<C-e>',
-          -- You might want to add these for navigation
           next = '<M-]>',
           prev = '<M-[>',
           dismiss = '<C-]>',
         },
       },
-      -- Other configuration options
       filetypes = {
-        -- Add any specific filetype configurations
-        jupyter = true, -- Enable for jupyter files
-        -- Add other filetypes as needed
+        jupyter = true,
       },
     }
-    -- Set up the toggle keymap using the built-in command
     vim.keymap.set('n', '<leader>tt', '<cmd>Copilot toggle<CR>', { noremap = true, silent = true, desc = 'Toggle Copilot' })
   end,
 }
