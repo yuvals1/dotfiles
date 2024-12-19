f145f0064db349161a1bcb1a1aef9197abe6c299
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-12-19 08:46:14 +0200

    feat(nvim/to-check-plugins): treewalker

---
+++ .config/nvim/lua/plugins/to-check/treewalker.lua
@@ -0,0 +1,7 @@
+return {
+  'aaronik/treewalker.nvim',
+  opts = {
+    highlight = true, -- Whether to briefly highlight the node after jumping to it
+    highlight_duration = 250, -- How long should above highlight last (in ms)
+  },
+}
