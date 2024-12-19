9e50eca1f1194c7e21adbbfc46a413c19794040a
Yuval Spiegel <yuvalspiegel1@gmail.com>
2024-12-19 18:26:28 +0200

    refactor(keymaps): update time insertion formats and add insert mode support
    - Change date format for <leader>t1 to use DD/MM/YYYY with markdown heading
    - Comment out unused time format mappings
    - Add new time insertion mappings for normal and insert modes using HHMM format

--- .config/nvim/lua/keymaps/time.lua
+++ .config/nvim/lua/keymaps/time.lua
@@ -10,8 +10,20 @@ local function insert_time(format)
 end
 
 -- Define mappings
-vim.keymap.set('n', '<leader>t1', insert_time '%H:%M', { desc = 'Insert time (HH:MM:SS)' })
-vim.keymap.set('n', '<leader>t2', insert_time '%Y-%m-%d', { desc = 'Insert date (YYYY-MM-DD)' })
-vim.keymap.set('n', '<leader>t3', insert_time '%Y-%m-%d %H:%M:%S', { desc = 'Insert date and time' })
-vim.keymap.set('n', '<leader>t4', insert_time '%c', { desc = 'Insert full date and time' })
-vim.keymap.set('n', '<leader>t5', insert_time '%a %b %d %Y', { desc = 'Insert date (Day Mon DD YYYY)' })
+vim.keymap.set('n', '<leader>t1', insert_time '## %d/%m/%Y', { desc = 'insert date' })
+-- vim.keymap.set('n', '<leader>t2', insert_time '%Y-%m-%d', { desc = 'Insert date (YYYY-MM-DD)' })
+-- vim.keymap.set('n', '<leader>t3', insert_time '%Y-%m-%d %H:%M:%S', { desc = 'Insert date and time' })
+-- vim.keymap.set('n', '<leader>t4', insert_time '%c', { desc = 'Insert full date and time' })
+-- vim.keymap.set('n', '<leader>t5', insert_time '%a %b %d %Y', { desc = 'Insert date (Day Mon DD YYYY)' })
+
+-- For normal mode
+vim.keymap.set('n', '<leader>t', function()
+  local time = os.date '%H%M'
+  vim.api.nvim_put({ time }, 'c', true, true)
+end, { desc = 'Insert current time' })
+
+-- For insert mode
+vim.keymap.set('i', '<C-t>', function()
+  local time = os.date '%H%M'
+  vim.api.nvim_put({ time }, 'c', true, true)
+end, { desc = 'Insert current time' })
