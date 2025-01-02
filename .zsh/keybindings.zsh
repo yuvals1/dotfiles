0e9141511ad3d7da848cb15ed83374521af9428f
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-01-02 12:54:50 +0200

    feat(keybindings): add Ctrl-U and Ctrl-G shortcuts
    - Add Ctrl-U to delete from cursor to start of line
    - Add Ctrl-G to move cursor to end of line
    
    This enhances terminal navigation capabilities with commonly used shortcuts.

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -1,6 +1,5 @@
 # Emacs style key bindings
 bindkey -e
-
 # Custom keybindings
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
@@ -8,3 +7,5 @@ bindkey '^[w' kill-region
 bindkey '^D' autosuggest-accept  # Ctrl-D to accept entire suggestion
 bindkey '^E' forward-word        # Ctrl-S to accept next word
 bindkey '^Y' yank-line-to-clipboard
+bindkey '^U' backward-kill-line
+bindkey '^G' end-of-line
