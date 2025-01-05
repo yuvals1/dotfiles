3682b97be0deb9c35bdfb089924727d4aa73a5c4
yuval <yuvalspiegel1@gmail.com>
2025-01-05 11:28:08 +0200

    not sure

--- .config/nvim/lua/keymaps/yank.lua
+++ .config/nvim/lua/keymaps/yank.lua
@@ -1,17 +1,28 @@
 vim.keymap.set('n', 'yf', ':%y<CR>', { noremap = true, silent = true, desc = 'Yank entire file' })
 
--- Yank absolute path (full path)
+-- Helper function to handle yanking with fallback
+local function yank_with_fallback(content, description)
+  -- Try system clipboard first
+  local success = pcall(function()
+    vim.fn.setreg('+', content)
+  end)
+  -- Fallback to unnamed register if system clipboard fails
+  if not success then
+    vim.fn.setreg('"', content)
+  end
+  vim.notify(description .. content, vim.log.levels.INFO)
+end
+
+-- Yank absolute path
 vim.keymap.set('n', 'yp', function()
   local file_path = vim.fn.expand '%:p'
-  vim.fn.setreg('+', file_path)
-  vim.notify('Yanked absolute path: ' .. file_path, vim.log.levels.INFO)
+  yank_with_fallback(file_path, 'Yanked absolute path: ')
 end, { noremap = true, desc = 'Yank absolute file path' })
 
 -- Yank file name only
 vim.keymap.set('n', 'yn', function()
   local file_name = vim.fn.expand '%:t'
-  vim.fn.setreg('+', file_name)
-  vim.notify('Yanked file name: ' .. file_name, vim.log.levels.INFO)
+  yank_with_fallback(file_name, 'Yanked file name: ')
 end, { noremap = true, desc = 'Yank file name' })
 
 -- Yank path relative to home directory
@@ -19,6 +30,5 @@ vim.keymap.set('n', 'yr', function()
   local abs_path = vim.fn.expand '%:p'
   local home = vim.fn.expand '$HOME'
   local rel_path = abs_path:gsub(home .. '/', '')
-  vim.fn.setreg('+', rel_path)
-  vim.notify('Yanked home-relative path: ' .. rel_path, vim.log.levels.INFO)
+  yank_with_fallback(rel_path, 'Yanked home-relative path: ')
 end, { noremap = true, desc = 'Yank path relative to home' })
