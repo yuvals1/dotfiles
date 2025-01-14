e44bef9211f79e4eeba4d2290f7f4d6d365e9957
ronen-eshel <ronen.eshel@treex-robotics.com>
2025-01-14 07:42:21 +0200

    feat(nvim): add indent like in main

--- .config/nvim/lua/keymaps/indent.lua
+++ .config/nvim/lua/keymaps/indent.lua
@@ -7,10 +7,12 @@ vim.keymap.set('n', '<C-t>', '>>', { noremap = true, desc = 'Indent line' })
 -- This is already built into Neovim, but you can explicitly set it if you want:
 vim.keymap.set('i', '<C-t>', '<C-t>', { noremap = true, desc = 'Indent line' })
 
--- Visual mode (optional): indent selected text
-vim.keymap.set('v', '<C-t>', '>', { noremap = true, desc = 'Indent selection' })
+-- Visual mode: indent and maintain selection
+vim.keymap.set('v', '<C-t>', '>gv', { noremap = true, desc = 'Indent selection' })
 
 -- Reverse indentation with CTRL-G
 vim.keymap.set('n', '<C-g>', '<<', { noremap = true, desc = 'Reverse indent line' })
 vim.keymap.set('i', '<C-g>', '<C-o><<', { noremap = true, desc = 'Reverse indent line' })
-vim.keymap.set('v', '<C-g>', '<', { noremap = true, desc = 'Reverse indent selection' })
+
+-- Visual mode: reverse indent and maintain selection
+vim.keymap.set('v', '<C-g>', '<gv', { noremap = true, desc = 'Reverse indent selection' })
