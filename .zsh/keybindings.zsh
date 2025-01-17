92d6236e475c4b77c3226db18bdaf45d3c1a4fdd
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-01-17 20:42:54 +0200

    fix: del output ~ in terminals

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -9,3 +9,4 @@ bindkey '^E' forward-word        # Ctrl-S to accept next word
 bindkey '^Y' yank-line-to-clipboard
 bindkey '^U' backward-kill-line
 bindkey '^G' end-of-line
+bindkey "\e[3~" delete-char
