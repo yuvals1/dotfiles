2123f7e6d711030a18104d36087dd13cee9cfa4d
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-01-19 00:15:29 +0200

    feat(yazi-plugins/selected-files-fzf): first working verison

--- .config/yazi/plugins/selected-files-fzf.yazi/main.lua
+++ .config/yazi/plugins/selected-files-fzf.yazi/main.lua
@@ -1,20 +1,36 @@
-local state = ya.sync(function()
-  return cx.active.current.cwd
-end)
-
 local function fail(s, ...)
-  ya.notify { title = 'Fzf', content = string.format(s, ...), timeout = 5, level = 'error' }
+  ya.notify { title = 'Selected-Fzf', content = string.format(s, ...), timeout = 5, level = 'error' }
 end
+
+-- Get selected files
+local get_selected = ya.sync(function()
+  local files = {}
+  for _, file in pairs(cx.active.selected) do
+    table.insert(files, tostring(file))
+  end
+  return files
+end)
 
 local function entry()
   local _permit = ya.hide()
-  local cwd = tostring(state())
 
-  local child, err = Command('fzf'):cwd(cwd):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
+  -- Get selected files
+  local files = get_selected()
+  if #files == 0 then
+    return fail 'No files selected'
+  end
+
+  -- Same fzf command but with PIPED stdin
+  local child, err = Command('fzf'):stdin(Command.PIPED):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
 
   if not child then
     return fail('Failed to start `fzf`, error: ' .. err)
   end
+
+  -- Write selected files to fzf
+  local input = table.concat(files, '\n')
+  child:write_all(input)
+  child:flush()
 
   local output, err = child:wait_with_output()
   if not output then
@@ -25,7 +41,7 @@ local function entry()
 
   local target = output.stdout:gsub('\n$', '')
   if target ~= '' then
-    ya.manager_emit(target:find '[/\\]$' and 'cd' or 'reveal', { target })
+    ya.manager_emit(target:find('[/\\]$' and 'cd' or 'reveal', { target }))
   end
 end
 
