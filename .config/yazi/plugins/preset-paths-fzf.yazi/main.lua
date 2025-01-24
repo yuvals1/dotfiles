560fe279459e9f70fa35c287c4e2942969a8ba5b
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-01-24 22:48:02 +0200

    show correct paths in fzf  but preview not working

--- .config/yazi/plugins/preset-paths-fzf.yazi/main.lua
+++ .config/yazi/plugins/preset-paths-fzf.yazi/main.lua
@@ -1,6 +1,4 @@
--- Define the folders at the top
--- local folders = '~/dev-projects/test-fzf-folders/a ~/dev-projects/test-fzf-folders/b'
-local folders = '~/treex-mono/robopilot/'
+local folders = '~/dev-projects/test-fzf-folders/a ~/dev-projects/test-fzf-folders/b'
 
 local state = ya.sync(function()
   return cx.active.current.cwd
@@ -13,23 +11,34 @@ end
 local function entry()
   local _permit = ya.hide()
   local cwd = tostring(state())
-  local child, err = Command('sh')
-    :args({ '-c', 'find ' .. folders .. ' -type f | fzf --delimiter="/" --with-nth=-1' })
-    :cwd(cwd)
-    :stdin(Command.INHERIT)
-    :stdout(Command.PIPED)
-    :stderr(Command.INHERIT)
-    :spawn()
+
+  -- Store find results in a variable first
+  local find_cmd = [[
+    result=$(for dir in ]] .. folders .. [[; do
+      (cd "$dir" && find . -type f | sed 's|^./||')
+    done)
+    
+    if [ -n "$result" ]; then
+      echo "$result" | fzf --preview "bat {}"
+    else
+      echo "No files found" >&2
+      exit 1
+    fi
+  ]]
+
+  local child, err = Command('sh'):args({ '-c', find_cmd }):cwd(cwd):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
 
   if not child then
-    return fail('Failed to start `fzf`, error: ' .. err)
+    return fail('Failed to start command, error: ' .. err)
   end
+
   local output, err = child:wait_with_output()
   if not output then
-    return fail('Cannot read `fzf` output, error: ' .. err)
+    return fail('Cannot read output, error: ' .. err)
   elseif not output.status.success and output.status.code ~= 130 then
-    return fail('`fzf` exited with error code %s', output.status.code)
+    return fail('Command exited with error code %s', output.status.code)
   end
+
   local target = output.stdout:gsub('\n$', '')
   if target ~= '' then
     ya.manager_emit(target:find '[/\\]$' and 'cd' or 'reveal', { target })
