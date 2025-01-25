6cd85f76404292c7a708c49978ed653d4dfe8e97
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-01-25 14:43:17 +0200

    refactor(fzf): remove custom dracula color scheme

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -62,10 +62,6 @@ export FZF_DEFAULT_OPTS="
   --preview='bat -n --color=always {}'
   --bind shift-up:preview-page-up,shift-down:preview-page-down
   --ansi
-  --color='fg:#f8f8f2,bg:#282a36,hl:#bd93f9'
-  --color='fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9'
-  --color='info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6'
-  --color='marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
 "
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
