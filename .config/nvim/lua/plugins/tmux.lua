c7e4fa926679664ef7324717fad155f89fddda13
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-02-04 08:36:25 +0200

    comment tmux.nvim

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -1,27 +1,27 @@
 return {
-  'aserowy/tmux.nvim',
-  event = 'VeryLazy',
-  config = function()
-    return require('tmux').setup {
-      copy_sync = {
-        enable = true,
-        redirect_to_clipboard = false,
-        register_offset = 0,
-        sync_clipboard = true,
-        sync_registers = true,
-        sync_deletes = true,
-        sync_unnamed = true,
-      },
-      navigation = {
-        cycle_navigation = true,
-        enable_default_keybindings = true,
-        persist_zoom = false,
-      },
-      resize = {
-        enable_default_keybindings = true,
-        resize_step_x = 1,
-        resize_step_y = 1,
-      },
-    }
-  end,
+  -- 'aserowy/tmux.nvim',
+  -- event = 'VeryLazy',
+  -- config = function()
+  --   return require('tmux').setup {
+  --     copy_sync = {
+  --       enable = true,
+  --       redirect_to_clipboard = false,
+  --       register_offset = 0,
+  --       sync_clipboard = true,
+  --       sync_registers = true,
+  --       sync_deletes = true,
+  --       sync_unnamed = true,
+  --     },
+  --     navigation = {
+  --       cycle_navigation = true,
+  --       enable_default_keybindings = true,
+  --       persist_zoom = false,
+  --     },
+  --     resize = {
+  --       enable_default_keybindings = true,
+  --       resize_step_x = 1,
+  --       resize_step_y = 1,
+  --     },
+  --   }
+  -- end,
 }
