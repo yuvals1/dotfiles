d5bfdce41886676e9b72a495ff57ea1861c16fcf
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-03-01 11:41:52 +0200

    remove yamb plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -22,18 +22,6 @@ if ya.target_family() == 'windows' then
   })
 end
 
-require('yamb'):setup {
-  -- Optional, the path ending with path seperator represents folder.
-  bookmarks = bookmarks,
-  -- Optional, recieve notification everytime you jump.
-  jump_notify = true,
-  -- Optional, the cli of fzf.
-  cli = 'fzf',
-  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
-  keys = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  -- Optional, the path of bookmarks
-  path = (ya.target_family() == 'windows' and os.getenv 'APPDATA' .. '\\yazi\\config\\bookmark') or (os.getenv 'HOME' .. '/.config/yazi/bookmark'),
-}
 
 function Linemode:justname()
   return self._file.name
