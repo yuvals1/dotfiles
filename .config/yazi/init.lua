45879ad2cbbf42234934d913964bcd7b292b4053
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-03-30 21:12:10 +0300

    add simple-tag configuration

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -22,7 +22,6 @@ if ya.target_family() == 'windows' then
   })
 end
 
-
 function Linemode:justname()
   return self._file.name
 end
@@ -59,3 +58,51 @@ function Linemode:lines()
     return string.format('%.1fK', count / 1000) .. 'L'
   end
 end
+
+require('simple-tag'):setup {
+  -- UI display mode (icon, text, hidden)
+  ui_mode = 'icon', -- (Optional)
+
+  -- Disable tag key hints (popup in bottom right corner)
+  hints_disabled = false, -- (Optional)
+
+  -- linemode order: adjusts icon/text position. Fo example, if you want icon to be on the mose left of linemode then set linemode_order larger than 1000.
+  -- More info: https://github.com/sxyazi/yazi/blob/077faacc9a84bb5a06c5a8185a71405b0cb3dc8a/yazi-plugin/preset/components/linemode.lua#L4-L5
+  linemode_order = 500, -- (Optional)
+
+  -- You can backup/restore this folder. But don't use backed up folder in the different OS.
+  -- save_path =  -- full path to save tags folder (Optional)
+  --       - Linux/MacOS: os.getenv("HOME") .. "/.config/yazi/tags"
+  --       - Windows: os.getenv("APPDATA") .. "\\yazi\\config\\tags"
+
+  -- Set tag colors
+  colors = { -- (Optional)
+    -- Set this same value with `theme.toml` > [manager] > hovered > reversed
+    -- Default theme use "reversed = true".
+    -- More info: https://github.com/sxyazi/yazi/blob/077faacc9a84bb5a06c5a8185a71405b0cb3dc8a/yazi-config/preset/theme-dark.toml#L25
+    reversed = true, -- (Optional)
+
+    -- More colors: https://yazi-rs.github.io/docs/configuration/theme#types.color
+    -- format: [tag key] = "color"
+    ['*'] = '#bf68d9', -- (Optional)
+    ['$'] = 'green',
+    ['!'] = '#cc9057',
+    ['1'] = 'cyan',
+    ['p'] = 'red',
+  },
+
+  -- Set tag icons. Only show when ui_mode = "icon".
+  -- Any text or nerdfont icons should work
+  -- Default icon from mactag.yazi: ●; , , 󱈤
+  -- More icon from nerd fonts: https://www.nerdfonts.com/cheat-sheet
+  icons = { -- (Optional)
+    -- default icon
+    default = '󰚋',
+
+    -- format: [tag key] = "tag icon"
+    ['*'] = '*',
+    ['$'] = '',
+    ['!'] = '',
+    ['p'] = '',
+  },
+}
