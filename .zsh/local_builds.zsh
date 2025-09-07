c6b5601d0666ad84ce2bbdc0c2f903d011080576
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-09-07 18:58:49 +0300

    update local build (remove lazy git and improve yazi)

--- .zsh/local_builds.zsh
+++ .zsh/local_builds.zsh
@@ -1,8 +1,8 @@
 # Local development builds
 # These aliases point to locally built versions of tools
 
-alias lazygit='/Users/yuvalspiegel/dev/lazygit/lazygit'
+# alias lazygit='/Users/yuvalspiegel/dev/lazygit/lazygit'
 
 # Define yazi binary path as a variable
-YAZI_BIN='/Users/yuvalspiegel/dev/yazi-nested/target/release/yazi'
+YAZI_BIN='/Users/yuvalspiegel/dev/yazi/target/release/yazi'
 alias yazi="$YAZI_BIN"
