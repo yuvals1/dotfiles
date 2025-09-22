f372968ca7b10b941467c40ff06f47464b434b8a
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-09-22 15:51:49 +0300

    yazi in nvim

--- .config/nvim/lua/keymaps/general.lua
+++ .config/nvim/lua/keymaps/general.lua
@@ -48,3 +48,44 @@ end, { desc = 'Toggle line wrapping' })
 -- This makes j/k behave more intuitively with wrapped lines
 vim.keymap.set({ 'n', 'v' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, desc = 'Move down by visual line' })
 vim.keymap.set({ 'n', 'v' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, desc = 'Move up by visual line' })
+
+-- Open Yazi in a full-screen terminal tab and auto-close on exit
+local function open_yazi_tab()
+  if vim.fn.executable('yazi') ~= 1 then
+    vim.notify('yazi not found on PATH', vim.log.levels.ERROR)
+    return
+  end
+
+  local dir
+  local bufname = vim.api.nvim_buf_get_name(0)
+  if bufname ~= '' then
+    dir = vim.fn.fnamemodify(bufname, ':p:h')
+  else
+    dir = vim.loop.cwd()
+  end
+
+  vim.cmd('tabnew')
+  local term_buf = vim.api.nvim_get_current_buf()
+  local chan = vim.fn.termopen('yazi', { cwd = dir })
+  if chan <= 0 then
+    vim.notify('failed to start yazi', vim.log.levels.ERROR)
+    if vim.fn.tabpagenr('$') > 1 then vim.cmd('tabclose') end
+    return
+  end
+  vim.cmd('startinsert')
+
+  vim.api.nvim_create_autocmd('TermClose', {
+    buffer = term_buf,
+    once = true,
+    callback = function()
+      if vim.fn.tabpagenr('$') > 1 then
+        vim.cmd('tabclose')
+      else
+        vim.cmd('bdelete!')
+      end
+    end,
+  })
+end
+
+vim.api.nvim_create_user_command('YaziStandalone', open_yazi_tab, { desc = 'Open Yazi in a terminal tab' })
+vim.keymap.set('n', 'g-', open_yazi_tab, { desc = 'Open Yazi (terminal tab)' })
